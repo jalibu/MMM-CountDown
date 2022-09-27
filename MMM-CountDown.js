6f94dbbf60e446337fc12a1df8ff37227b088817
Jan Litzenburger <jan.litzenburger@sap.com>
2022-09-28 00:34:41 +0200

    adapt

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -1,62 +1,69 @@
-Module.register("MMM-CountDown",{
-	// Default module config.
-	defaults: {
-		event: "New Millenium:",
-		date: "3000-01-01",
-		showHours: true,
-		showMinutes: true,
-		showSeconds: true,
-		customInterval: 1000,
-		daysLabel: 'd',
-		hoursLabel: 'h',
-		minutesLabel: 'm',
-		secondsLabel: 's',
-	},
+Module.register("MMM-CountDown", {
+  // Default module config.
+  defaults: {
+    event: "New Millenium:",
+    date: "3000-01-01",
+    showHours: true,
+    showMinutes: true,
+    showSeconds: true,
+    customInterval: 1000 * 60 * 60,
+    daysLabel: "d",
+    hoursLabel: "h",
+    minutesLabel: "m",
+    secondsLabel: "s"
+  },
 
-	// set update interval
-	start: function() {
-		var self = this;
-		setInterval(function() {
-			self.updateDom(); // no speed defined, so it updates instantly.
-		}, this.config.customInterval); 
-	},
+  // set update interval
+  start: function () {
+    var self = this;
+    setInterval(function () {
+      self.updateDom(); // no speed defined, so it updates instantly.
+    }, this.config.customInterval);
+  },
 
-	// Update function
-	getDom: function() {
-		var wrapper = document.createElement("div");
+  // Update function
+  getDom: function () {
+    var wrapper = document.createElement("div");
 
-		var timeWrapper = document.createElement("div");
-		var textWrapper = document.createElement("div");
+    var timeWrapper = document.createElement("div");
+    var textWrapper = document.createElement("div");
 
-		textWrapper.className = "align-left week dimmed medium";
-		timeWrapper.className = "time bright xlarge light";
-		textWrapper.innerHTML=this.config.event;
+    textWrapper.className = "week dimmed medium";
+    timeWrapper.className = "time bright light";
+    textWrapper.innerHTML = this.config.event;
 
-		var today = new Date(Date.now());
-		var target = new Date(this.config.date);
-		var timeDiff = target - today;
+    var today = new Date(Date.now());
+    var target = new Date(this.config.date);
+    var timeDiff = target - today;
 
-		// Set days, hours, minutes and seconds
-		var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-		var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-		var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-		
-		// Build the output
-		var hrs = '';
-		var mins = '';
-		var secs = '';
-		var days = diffDays + this.config.daysLabel;
+    // Set days, hours, minutes and seconds
+    var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    var diffHours = Math.floor(
+      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-		if(this.config.showHours == true) hrs = diffHours + this.config.hoursLabel;
-		if(this.config.showMinutes == true) mins = diffMinutes + this.config.minutesLabel;
-		if(this.config.showSeconds == true) secs = diffSeconds + this.config.secondsLabel;
-
-		timeWrapper.innerHTML = days + hrs + mins + secs;
-
-		wrapper.appendChild(textWrapper);
-		wrapper.appendChild(timeWrapper);
-
-		return wrapper;
+	if(diffDays < 0){
+		diffDays = diffDays * -1
 	}
+    // Build the output
+    var hrs = "";
+    var mins = "";
+    var secs = "";
+    var days = diffDays + this.config.daysLabel;
+
+    if (this.config.showHours == true) hrs = diffHours + this.config.hoursLabel;
+    if (this.config.showMinutes == true)
+      mins = diffMinutes + this.config.minutesLabel;
+    if (this.config.showSeconds == true)
+      secs = diffSeconds + this.config.secondsLabel;
+
+    timeWrapper.innerHTML = days + hrs + mins + secs + "(" + (diffDays/7).toFixed(0) + " Wochen)";
+
+    wrapper.appendChild(textWrapper);
+    wrapper.appendChild(timeWrapper);
+
+    return wrapper;
+  }
 });
